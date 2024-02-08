669cc67aae077002f63b1c36e5ab1ca690d62fa3
Gustavo Benjamin <gustasbenjamin@gmail.com>
2024-02-08 00:47:11 -0300

    Desafio 2 completo

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
@@ -7,10 +8,13 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
-        print(http_request)
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
+
         # logica regra de negocio
-        print('estou na view')
+        formatted_response = tag_creator_controller.create(product_code)
+
         # retorno http
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
